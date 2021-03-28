24bd8ed63e006956611209435f58f70d032cb032
Marc-Antoine Ruel <maruel@gmail.com>
2021-03-28 12:35:44 -0400

    Increase code coverage to 97%

--- ansi256_test.go
+++ ansi256_test.go
@@ -6,13 +6,14 @@ package ansi256
 
 import (
 	"image/color"
+	"strconv"
 	"testing"
 )
 
 func TestANSI(t *testing.T) {
 	data := []struct {
-		expected int
-		c        color.NRGBA
+		want int
+		c    color.NRGBA
 	}{
 		{0, color.NRGBA{}},
 		{0, color.NRGBA{1, 1, 1, 255}},
@@ -22,8 +23,62 @@ func TestANSI(t *testing.T) {
 		{255, color.NRGBA{0xE5, 0xEE, 0xF0, 255}},
 	}
 	for i, line := range data {
-		if v := Term256.ANSI(line.c); v != line.expected {
-			t.Fatalf("%d: Term256.ANSI(%v) = %d, expected %d", i, line.c, v, line.expected)
-		}
+		line := line
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if v := Term256.ANSI(line.c); v != line.want {
+				t.Fatalf("Term256.ANSI(%v) = %d, want %d", line.c, v, line.want)
+			}
+		})
+	}
+}
+
+func TestRaw(t *testing.T) {
+	data := []struct {
+		foreground bool
+		c          color.NRGBA
+		want       string
+	}{
+		{
+			true,
+			color.NRGBA{255, 0, 0, 255},
+			"\x1b[38;2;255;0;0m ",
+		},
+		{
+			false,
+			color.NRGBA{255, 0, 0, 255},
+			"\x1b[48;2;255;0;0m ",
+		},
+	}
+	for i, line := range data {
+		line := line
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := Raw(line.foreground, line.c); got != line.want {
+				t.Fatalf("Raw(%t, %v) = %q, want %q", line.foreground, line.c, got, line.want)
+			}
+		})
+	}
+}
+
+func TestBlock(t *testing.T) {
+	data := []struct {
+		c    color.NRGBA
+		want string
+	}{
+		{
+			color.NRGBA{255, 0, 0, 255},
+			"\x1b[48;5;9m ",
+		},
+		{
+			color.NRGBA{255, 10, 10, 255},
+			"\x1b[48;5;9m\x1b[38;5;196m▒",
+		},
+	}
+	for i, line := range data {
+		line := line
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if got := Term256.Block(line.c); got != line.want {
+				t.Fatalf("Block(%v) = %q, want %q", line.c, got, line.want)
+			}
+		})
 	}
 }
